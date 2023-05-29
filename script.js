6b21e568332060d0436e19b8f1321459a40a9f1b
pavblw <gualapab@gmail.com>
2023-05-29 02:23:14 -0300

    v4

--- script.js
+++ script.js
@@ -5,96 +5,95 @@ let products = {
       nombreYautor: "La niña que no veían - Gaston Rosa",
       categoria: "Infantil",
       codigo: "INNVGR2018",
-      portada:
-        "INNVGR2018.webp",
-      prologo: "Algunas veces Martina siente que la tratan diferente y no entiende cuál es la razón. Conociendo otras realidades, como la de Martina, es que aprendemos a respetar y a valorar la diversidad. Celebrar las diferencias en los demás, es también aceptar las propias. ¡Lo mismo que te hace diferente, te hace insuperable!",
+      portada: "INNVGR2018.webp",
+      prologo:
+        "Algunas veces Martina siente que la tratan diferente y no entiende cuál es la razón. Conociendo otras realidades, como la de Martina, es que aprendemos a respetar y a valorar la diversidad. Celebrar las diferencias en los demás, es también aceptar las propias. ¡Lo mismo que te hace diferente, te hace insuperable!",
     },
     {
       nombreYautor: "Zapatos cambiados  - Gaston Rosa",
       categoria: "Infantil",
       codigo: "IZCGR2018",
-      portada:
-        "IZCGR2018.webp",
-      prologo: "Este es el cuento sobre como dos hermanitos cambian la historia de un pueblo. Se les ocurre una idea que se vuelve la nueva tradicción del lugar. Ponerse en los zapatos del otro es comprender, respetar y valorar la diversidad. Y eso también sirve para que puedas aceptarte a ti mismo. ¡Lo mismo que te hace diferente, te hace insuperable!",
+      portada: "IZCGR2018.webp",
+      prologo:
+        "Este es el cuento sobre como dos hermanitos cambian la historia de un pueblo. Se les ocurre una idea que se vuelve la nueva tradicción del lugar. Ponerse en los zapatos del otro es comprender, respetar y valorar la diversidad. Y eso también sirve para que puedas aceptarte a ti mismo. ¡Lo mismo que te hace diferente, te hace insuperable!",
     },
     {
       nombreYautor: "La niña que tenía dos papás - Mel Elliott",
       categoria: "Infantil",
       codigo: "INTDPME2019",
-      portada:
-        "INTDPME2019.webp",
-      prologo: "Berta está emocionada con Matilda, la niña nueva que ha llegado a su colegio; ¡es igualita a ella! Solo se diferencian en una cosa: Matilda tiene dos papás. Berta está deseando conocerlos, ¡seguro que en su casa todo es muy diferente!",
+      portada: "INTDPME2019.webp",
+      prologo:
+        "Berta está emocionada con Matilda, la niña nueva que ha llegado a su colegio; ¡es igualita a ella! Solo se diferencian en una cosa: Matilda tiene dos papás. Berta está deseando conocerlos, ¡seguro que en su casa todo es muy diferente!",
     },
     {
       nombreYautor: "El vestido más lindo del mundo - Monica dos Santos",
       categoria: "Infantil",
       codigo: "IVMLMMDS2016",
-      portada:
-        "IVMLMMDS2016.webp",
-      prologo: "Una niña llamada Naiat recibe de su madre un vestido que no le gusta, por lo que recorre las casas de sus vecinas y sus amigas buscando ayuda. Las mujeres con las que se encuentra la van decorando con zapatos, un peinado, accesorios y detalles, hasta que, finalmente, siente que tiene «el vestido más lindo del mundo». La niña termina con un vestido que le gusta, repleto de valores y riquezas colectivas, porque encontró contención y apoyo en la gente que la rodea.",
+      portada: "IVMLMMDS2016.webp",
+      prologo:
+        "Una niña llamada Naiat recibe de su madre un vestido que no le gusta, por lo que recorre las casas de sus vecinas y sus amigas buscando ayuda. Las mujeres con las que se encuentra la van decorando con zapatos, un peinado, accesorios y detalles, hasta que, finalmente, siente que tiene «el vestido más lindo del mundo». La niña termina con un vestido que le gusta, repleto de valores y riquezas colectivas, porque encontró contención y apoyo en la gente que la rodea.",
     },
     {
       nombreYautor: "El vestido de mamá  - Dani Umpi",
       categoria: "Infantil",
       codigo: "IEVDMDU2011",
-      portada:
-        "IEVDMDU2011.webp",
-      prologo: "Un niño esta encantado con el vestido verde que su mamá usa para ir a las fiestas. Se imagina otra ropa con esa tela mágica. Un día decide ponerse el vestido sobre su equipo de futbol y sale a jugar con sus amigos. En la plaza se encuentra con que hay algunos códigos que no son tan fáciles de entender. Y todavía tiene que volver a casa con el vestido cubierto de barro…",
+      portada: "IEVDMDU2011.webp",
+      prologo:
+        "Un niño esta encantado con el vestido verde que su mamá usa para ir a las fiestas. Se imagina otra ropa con esa tela mágica. Un día decide ponerse el vestido sobre su equipo de futbol y sale a jugar con sus amigos. En la plaza se encuentra con que hay algunos códigos que no son tan fáciles de entender. Y todavía tiene que volver a casa con el vestido cubierto de barro…",
     },
     {
       nombreYautor: "Susy Shock para chixs - Nadia Fink y Pitu Saá",
       categoria: "Infantil",
       codigo: "ISSPCNFPS2018",
-      portada:
-        "ISSPCNFPS2018.webp",
-      prologo: "Esta es una historia llena de abrazos porque a Susy Shock, la protagonista, la abrazaron desde pequeña en sus múltiples formas de sentir y de pensar. Ella se define como travesti o, mejor dicho, como “trava”. ¿Y qué significa eso? Que eligió ser como siente. Susy nos cuenta su vida y verás qué feliz se siente y qué segura de sí misma. Nuestra Antiprincesa Colibrí nos habla de todas las posibilidades de ser y de soñar.",
+      portada: "ISSPCNFPS2018.webp",
+      prologo:
+        "Esta es una historia llena de abrazos porque a Susy Shock, la protagonista, la abrazaron desde pequeña en sus múltiples formas de sentir y de pensar. Ella se define como travesti o, mejor dicho, como “trava”. ¿Y qué significa eso? Que eligió ser como siente. Susy nos cuenta su vida y verás qué feliz se siente y qué segura de sí misma. Nuestra Antiprincesa Colibrí nos habla de todas las posibilidades de ser y de soñar.",
     },
     {
       nombreYautor: "Un apartamento en Urano - Paul B. Preciado",
       categoria: "TeoríaQueer",
       codigo: "TUAEUPBP2019",
-      portada:
-        "TUAEUPBP2019.webp",
-      prologo: "Urano, el gigante helado, es el planeta más frío del sistema solar, y también un dios de la mitología griega. Urano da además nombre al uranismo, concepto forjado por el primer activista sexual europeo, Karl-Heinrich Ulrichs, en 1864 para definir el «tercer sexo».",
+      portada: "TUAEUPBP2019.webp",
+      prologo:
+        "Urano, el gigante helado, es el planeta más frío del sistema solar, y también un dios de la mitología griega. Urano da además nombre al uranismo, concepto forjado por el primer activista sexual europeo, Karl-Heinrich Ulrichs, en 1864 para definir el «tercer sexo».",
     },
     {
       nombreYautor: "Testo yonqui - Paul B. Preciado",
       categoria: "TeoríaQueer",
       codigo: "TTYPBP2008",
-      portada:
-        "TTYPBP2008.webp",
-      prologo: "El modelo capitalista actual se asienta en dos pilares fundamentales: la industria farmacéutica y la pornografía. En paralelo al discurso filosófico que analiza esta realidad farmacopornográfica, relata un autoexperimento que le lleva a aplicarse testosterona, para desmentir con él la determinación biólógica del género y la sexualidad.",
+      portada: "TTYPBP2008.webp",
+      prologo:
+        "El modelo capitalista actual se asienta en dos pilares fundamentales: la industria farmacéutica y la pornografía. En paralelo al discurso filosófico que analiza esta realidad farmacopornográfica, relata un autoexperimento que le lleva a aplicarse testosterona, para desmentir con él la determinación biólógica del género y la sexualidad.",
     },
     {
       nombreYautor: "Manifiesto contrasexual - Paul B. Preciado",
       categoria: "TeoríaQueer",
       codigo: "TMCPBP2011",
-      portada:
-        "TMCPBP2011.webp",
-      prologo: "Provocativa relectura de la sexualidad a través de los trabajos de Deleuze y Foucault, también en la línea del feminismo materialista de Wittig y de las investigaciones sobre el género de Butler y Derrida.",
+      portada: "TMCPBP2011.webp",
+      prologo:
+        "Provocativa relectura de la sexualidad a través de los trabajos de Deleuze y Foucault, también en la línea del feminismo materialista de Wittig y de las investigaciones sobre el género de Butler y Derrida.",
     },
     {
       nombreYautor: "Pornotopía - Paul B. Preciado",
       categoria: "TeoríaQueer",
       codigo: "TPPBP2010",
-      portada:
-        "TPPBP2010.webp",
-      prologo: "En plena guerra fría, Hugh Hefner crea la que pronto se convertiría en la revista para adultos más vendida del mundo: Playboy, que no era simplemente una revista de chicas con o sin bikini, sino un vasto proyecto arquitectónico-mediático que tenía como objetivo desplazar la casa heterosexual como núcleo de consumo y reproducción. De la misma manera que la sociedad ilustrada creyó que la celda individual podía ser un enclave de reconstrucción del alma criminal, Playboy confió a la mansión de soltero la fabricación del nuevo hombre moderno.",
+      portada: "TPPBP2010.webp",
+      prologo:
+        "En plena guerra fría, Hugh Hefner crea la que pronto se convertiría en la revista para adultos más vendida del mundo: Playboy, que no era simplemente una revista de chicas con o sin bikini, sino un vasto proyecto arquitectónico-mediático que tenía como objetivo desplazar la casa heterosexual como núcleo de consumo y reproducción. De la misma manera que la sociedad ilustrada creyó que la celda individual podía ser un enclave de reconstrucción del alma criminal, Playboy confió a la mansión de soltero la fabricación del nuevo hombre moderno.",
     },
     {
       nombreYautor: "Utopía Queer - José E. Muñoz",
       categoria: "TeoríaQueer",
       codigo: "TUQJEM2009",
-      portada:
-        "TUQJEM2009.webp",
-      prologo: "Cuando el cubano José Esteban Muñoz publicó por primera vez este libro en el año 2009, la imaginación política LGTBIQ se encontraba erosionada por un pragmatismo asfixiante.",
+      portada: "TUQJEM2009.webp",
+      prologo:
+        "Cuando el cubano José Esteban Muñoz publicó por primera vez este libro en el año 2009, la imaginación política LGTBIQ se encontraba erosionada por un pragmatismo asfixiante.",
     },
     {
       nombreYautor: "Gays y derecha  - L. Gutierrez y D. Puntigliano ",
       categoria: "Periodístico",
       codigo: "PGYDLGDP2018",
-      portada:
-        "PGYDLGDP2018.webp",
+      portada: "PGYDLGDP2018.webp",
       prologo: "",
     },
     {
@@ -102,41 +101,41 @@ let products = {
       categoria: "Periodístico",
       codigo: "PIV2016",
       portada: nohay,
+      prologo: "",
     },
     {
       nombreYautor:
         "Amor libre: Interviews voluptuosos - Roberto de las Carreras",
       categoria: "Entrevistas",
       codigo: "EALIVRDLC1902",
-      portada:
-        "EALIVRDLC1902.webp",
+      portada: "EALIVRDLC1902.webp",
       prologo: "",
     },
     {
       nombreYautor: "Fuego Queer - Ed Mead y Rita Brown",
       categoria: "Historia",
       codigo: "HFQEMRB2014",
-      portada:
-        "HFQEMRB2014.webp",
-      prologo: "Historia de la \"Brigada George Jackson\" y del colectivo gay anticarcelario \"Hombres contra el sexismo\" (1975-1978). Brigada George Jackson aglutinó a anarquistas, comunistas, maricas, bolleras, heterosexuales, gente negra, blanca y de ascendencia nativa."
+      portada: "HFQEMRB2014.webp",
+      prologo:
+        'Historia de la "Brigada George Jackson" y del colectivo gay anticarcelario "Hombres contra el sexismo" (1975-1978). Brigada George Jackson aglutinó a anarquistas, comunistas, maricas, bolleras, heterosexuales, gente negra, blanca y de ascendencia nativa.',
     },
     {
       nombreYautor:
         "Acción Travesti Callejera Revolucionaria - Marsha Johnson y Sylvia Rivera",
       categoria: "Biografía",
       codigo: "HATCRMJSR2013",
-      portada:
-        "HATCRMJSR2013.webp",
-        prologo: "Recopilación de textos de Marsha P. Johnson y Sylvia Rivera, dos trans que vivieron en Nueva York en la época de los disturbios de Stonewall, que poco después iniciaron un proyecto autogestionado de apoyo mutuo para la gente trans más marginada, manteniendo un hogar de acogida. Bajo un discurso revolucionario y prácticas de acción directa, estas dos grandes personalidades de la lucha gay estadounidense mostraron una vía de lucha contra el patriarcado que comenzaba por defender a las personas sin diferenciarlas por cuestiones de clase, raza o cómo viven el género y la sexualidad."
+      portada: "HATCRMJSR2013.webp",
+      prologo:
+        "Recopilación de textos de Marsha P. Johnson y Sylvia Rivera, dos trans que vivieron en Nueva York en la época de los disturbios de Stonewall, que poco después iniciaron un proyecto autogestionado de apoyo mutuo para la gente trans más marginada, manteniendo un hogar de acogida. Bajo un discurso revolucionario y prácticas de acción directa, estas dos grandes personalidades de la lucha gay estadounidense mostraron una vía de lucha contra el patriarcado que comenzaba por defender a las personas sin diferenciarlas por cuestiones de clase, raza o cómo viven el género y la sexualidad.",
     },
     {
       nombreYautor:
         "Oscar Wilde y yo + De Profundis  - Alfred Douglas + Oscar Wild",
       categoria: "Biografía",
       codigo: "HOWYYDPADOW2019",
-      portada:
-        "HOWYYDPADOW2019.webp",
-      prologo: "Oscar Wilde y yo fue publicado en 1914, un año después del proceso. Se trata de un descargo contra la ola de demandas que los admiradores del escritor lanzaron contra Douglas y que no cesarían ni aun después de su muerte.",
+      portada: "HOWYYDPADOW2019.webp",
+      prologo:
+        "Oscar Wilde y yo fue publicado en 1914, un año después del proceso. Se trata de un descargo contra la ola de demandas que los admiradores del escritor lanzaron contra Douglas y que no cesarían ni aun después de su muerte.",
     },
     {
       nombreYautor: "Librillo SAT - III Edición  - Montevideo Igualitario",
@@ -171,13 +170,15 @@ let products = {
       categoria: "PolíticaPública",
       codigo: "PBPDSMM2019",
       portada: nohay,
-      prologo: "La última segregación. Baños públicos, moral, género y sexualidad en Uruguay.",
+      prologo:
+        "La última segregación. Baños públicos, moral, género y sexualidad en Uruguay.",
     },
     {
       nombreYautor: "Policlinicas inclusivas - Marcela Schenck",
       categoria: "PolíticaPública",
       codigo: "PPIMS2019",
       portada: nohay,
+      prologo: "",
     },
     {
       nombreYautor:
@@ -206,6 +207,7 @@ let products = {
       categoria: "Teoría",
       codigo: "TIDS2019",
       portada: nohay,
+      prologo: "",
     },
     {
       nombreYautor:
@@ -232,9 +234,10 @@ let products = {
   ],
 };
 
-document.getElementById('search-input').addEventListener('keyup', (event) => {
-  if (event.key === 'Enter') {
-    document.getElementById('search').click();
+//Funcionamiento
+document.getElementById("search-input").addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    document.getElementById("search").click();
   }
 });
 

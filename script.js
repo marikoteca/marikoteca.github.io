3dde5eb3e914c739f3395c30b706963af33a2a3c
pavblw <gualapab@gmail.com>
2023-06-05 02:23:27 -0300

    v5

--- script.js
+++ script.js
@@ -376,14 +376,8 @@ for (let i of products.data) {
     document.body.removeChild(el);
     alert("Código copiado 📋");
   });
-  codigo.addEventListener("touchstart", function(){
-    const el = document.createElement('textarea');
-    el.value = i.codigo;
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand("copy");
-    document.body.removeChild(el);
-    alert("Código copiado 📋");
+  codigo.addEventListener("mouseover", () => {
+    codigo.style.cursor = "pointer";
   });
   //prologo
   let prologoClicked = false;
